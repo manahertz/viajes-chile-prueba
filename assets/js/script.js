da03fa8c8f254956f5ad1264f69f3b5b3ff454e7
unknown <m.hertzacevedo@gmail.com>
2022-05-13 03:40:16 -0400

    se actualizo el js

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,8 @@ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
 
+// TOOLTIPS BOOTSTRAP //
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
