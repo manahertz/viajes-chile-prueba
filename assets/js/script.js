b78ebbda3675c939f436d9b7f65402ecc4a07123
unknown <m.hertzacevedo@gmail.com>
2022-05-13 03:38:52 -0400

    se agrgo js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// SCROLL //
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top - 50
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
+
+// CAMBIO COLOR NAVBAR //
+$(window).scroll(function(){
+    $("nav").toggleClass("scrolled",$(this).scrollTop()>70);
+});
+
+// POPOVERS BOOTSTRAP // 
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
